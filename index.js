937822525780d4993a6f8fc97d06043b10b6e2a7
aleem aheer <aleemaheer5769644@gmail.com>
2021-12-09 20:51:13 +0500

    Created a static server that just servers html and css files, that i created in node.js crash course

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+// Set static folder
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+})*/
 
 const PORT = process.env.PORT || 5000;
 
